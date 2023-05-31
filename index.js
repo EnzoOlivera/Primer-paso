d9469c9e710a41161c7e29442b60d29bd153dc87
EnzoOlivera <oliveraenzo04@gmail.com>
2023-05-31 19:28:47 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const miBoton = document.getElementById('miBoton');
+function handleClick() {
+    alert('¡Hola! Has hecho clic en mi boton.');
+}
+let boton = document.getElementById("miBoton");
+boton.addEventListener("click", function() {
+    alert("¡Haz hecho clic en mi boton!");
+});
